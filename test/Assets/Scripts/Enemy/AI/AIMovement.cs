f5af68470d003d62ea19ad2e4fbf2816ee821112
David Oliva <davidoliva89@hotmail.com>
2017-08-02 14:36:12 +1000

    Merger of snowy daves

---
+++ test/Assets/Scripts/Enemy/AI/AIMovement.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIMovement : MonoBehaviour {
+
+    private NavMeshAgent agent;
+    private Vector3 startPosition;
+    public bool wandering = true;
+    public bool chasing = false;
+
+    public float wanderingSpeed = 0.5f;
+    public float wanderRange = 5.0f;
+    public float chaseSpeed = 2.0f;
+
+    private GameObject player;
+
+    // Use this for initialization
+    void Awake ()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.speed = wanderingSpeed;
+        startPosition = this.transform.position;
+
+        player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+    void Update()
+    {
+        if (wandering != false && !IsInvoking("Wander"))
+        {
+            InvokeRepeating("Wander", 1f, 5f);
+        }
+        if(chasing)
+        {
+            ChasePlayer();
+        }
+    }
+
+    void Wander()
+    {
+        Vector3 destination = startPosition + new Vector3(Random.Range(-wanderRange, wanderRange), 0, Random.Range(-wanderRange, wanderRange));
+        NewDestination(destination);
+        agent.speed = wanderingSpeed;
+    }
+
+    public void NewDestination(Vector3 targetPoint)
+    {
+        agent.SetDestination(targetPoint);
+    }
+
+    void ChasePlayer()
+    {
+        //GetComponent<NavMeshAgent>().destination = player.transform.position;
+        NewDestination(player.transform.position);
+        agent.speed = chaseSpeed;
+    }
+
+    void OnTriggerEnter(Collider player)
+    {
+        if(player.tag == "Player")
+        {
+            wandering = false;
+            chasing = true;
+            CancelInvoke();
+        }
+    }
+
+    void OnTriggerExit(Collider player)
+    {
+        if(player.tag == "Player")
+        {
+            wandering = true;
+            chasing = false;
+        }
+    }
+}
---
+++ test/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerMovement : MonoBehaviour {
+
+    Rigidbody rb;
+    float player;
+    public float walkSpeed;
+    public float runSpeed;
+    // Use this for initialization
+
+    private Vector3 mousePosition;
+    private Vector3 direction;
+    private float distanceFromObject;
+    public Camera camera;
+
+    void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        //Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.z, 10);
+        //Vector3 lookPos = Camera.main.ScreenToWorldPoint(mousePos);
+        //lookPos = lookPos - transform.position;
+        //float angle = Mathf.Atan2(lookPos.z, lookPos.x) * Mathf.Rad2Deg;
+        //transform.rotation = Quaternion.AngleAxis(angle, Vector3.right);
+
+        walking(h, v);
+        turning();
+    }
+
+    void walking(float x, float v) {
+
+        string direction = "none";
+
+        if (v > 0.1) direction = "forward";
+        if (v < -0.1) direction = "backward";
+        if (x > 0.1) direction = "right";
+        if (x < -0.1) direction = "left";
+        if (v > 0.1 && x < -0.1) direction = "ForwardLeft";
+        if (v > 0.1 && x > 0.1) direction = "ForwardRight";
+        if (v < -0.1 && x < -0.1) direction = "BackwardLeft";
+        if (v < -0.1 && x > 0.1) direction = "BackwardRight";
+        switch (direction) {
+            case "forward":
+                rb.MovePosition(transform.position + transform.forward * walkSpeed * Time.deltaTime);
+                break;
+
+            case "backward":
+                rb.MovePosition(transform.position - transform.forward * walkSpeed * Time.deltaTime);
+                break;
+
+            case "right":
+                rb.MovePosition(transform.position + transform.right * walkSpeed * Time.deltaTime);
+                break;
+
+            case "left":
+                rb.MovePosition(transform.position - transform.right * walkSpeed * Time.deltaTime);
+                break;
+
+            case "ForwardLeft":
+                rb.MovePosition(transform.position - (transform.right - transform.forward) * walkSpeed * Time.deltaTime);
+                break;
+
+            case "ForwardRight":
+                rb.MovePosition(transform.position + (transform.right + transform.forward) * walkSpeed * Time.deltaTime);
+                break;
+
+            case "BackwardLeft":
+                rb.MovePosition(transform.position - (transform.right + transform.forward) * walkSpeed * Time.deltaTime);
+                break;
+
+            case "BackwardRight":
+                rb.MovePosition(transform.position + (transform.right - transform.forward) * walkSpeed * Time.deltaTime);
+                break;
+        }
+    }
+
+      void  turning() {
+        float mouseInput = Input.GetAxis("Mouse X");
+        Vector3 lookhere = new Vector3(0, mouseInput, 0);
+        transform.Rotate(lookhere);
+    }
+
+}
+
